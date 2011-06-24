92fe77995e1cb378ecb9dc1b668d03e77b4055fd
Tatsuhiko Miyagawa <miyagawa@bulknews.net>
2011-06-24 15:26:55 -0700

    foo

---
+++ wsgi.py
@@ -0,0 +1,15 @@
+from flask import Flask
+app = Flask(__name__)
+app.debug = True
+
+@app.route("/")
+def hello():
+    p()
+    return "Hello World foo!"
+
+from werkzeug.debug import DebuggedApplication
+application = DebuggedApplication(app, evalex=True)
+
+if __name__ == "__main__":
+    from werkzeug.serving import run_simple
+    run_simple('', 4000, application, use_debugger=True, use_evalex=True)
